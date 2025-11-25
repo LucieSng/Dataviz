2c2e45516f97545a58a39a5fcc25813965fe970b
Lucie Seng <lucie.sng@gmail.com>
2025-11-25 09:57:58 +0100

    chore : add import line chart

---
+++ src/assets/components/LineChartYear.tsx
@@ -0,0 +1,13 @@
+import { Line, LineChart } from "recharts";
+
+export default function Step1() {
+  return (
+    <LineChart
+      style={{ width: "100%", aspectRatio: 1.618, maxWidth: 600 }}
+      responsive
+      data={data}
+    >
+      <Line dataKey="uv" />
+    </LineChart>
+  );
+}
