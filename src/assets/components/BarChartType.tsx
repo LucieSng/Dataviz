a8871da6682396cd55df1ee3a96ded9cae4090c2
piouoiq <melargilliere@gmail.com>
2025-11-26 15:47:00 +0100

    add barCharType

---
+++ src/assets/components/BarChartType.tsx
@@ -0,0 +1,30 @@
+// // code pris ctrl C/V sur le site de recharts
+
+// import { BarChart, Bar } from "recharts";
+// import { useParisApi } from "../components/hooks/useParisApi";
+
+// // #region Sample data
+// const { apiData } = useParisApi();
+
+// // #endregion
+// const TinyBarChart = () => {
+//   return (
+//     <BarChart
+//       style={{
+//         width: "100%",
+//         maxWidth: "300px",
+//         maxHeight: "100px",
+//         aspectRatio: 1.618,
+//       }}
+//       responsive
+//       data={data}
+//     >
+//       <Bar dataKey="uv" fill="#8884d8" />
+//     </BarChart>
+//   );
+// };
+
+// export default TinyBarChart;
+// function useParisApi(): { apiData: any } {
+//   throw new Error("Function not implemented.");
+// }
