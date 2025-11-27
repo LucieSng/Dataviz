edca29b8ee1e17e7fc206b8b9add8d59f3d579cf
piouoiq <melargilliere@gmail.com>
2025-11-27 14:57:44 +0100

    feat: barChart base

--- src/assets/components/BarChartType.tsx
+++ src/assets/components/BarChartType.tsx
@@ -1,30 +1,37 @@
-// // code pris ctrl C/V sur le site de recharts
+// code pris ctrl C/V sur le site de recharts
 
-// import { BarChart, Bar } from "recharts";
-// import { useParisApi } from "../components/hooks/useParisApi";
+import { BarChart, Bar } from "recharts";
+import { useParisApi } from "../components/hooks/useParisApi";
 
-// // #region Sample data
-// const { apiData } = useParisApi();
+const BarChartType = ({ isAnimationActive = false }) => {
+  // On définit une URL spécifique pour ce graphique qui récupère les tournages groupés par année
+  const apiUrl =
+    "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/lieux-de-tournage-a-paris/records?select=annee_tournage,count(*)&group_by=annee_tournage&limit=100";
+  const { apiData } = useParisApi(apiUrl);
 
-// // #endregion
-// const TinyBarChart = () => {
-//   return (
-//     <BarChart
-//       style={{
-//         width: "100%",
-//         maxWidth: "300px",
-//         maxHeight: "100px",
-//         aspectRatio: 1.618,
-//       }}
-//       responsive
-//       data={data}
-//     >
-//       <Bar dataKey="uv" fill="#8884d8" />
-//     </BarChart>
-//   );
-// };
+  // Message pour le chargement des données
+  if (!apiData?.results) {
+    return <div>Chargement des données...</div>;
+  }
 
-// export default TinyBarChart;
-// function useParisApi(): { apiData: any } {
-//   throw new Error("Function not implemented.");
-// }
+  // #endregion
+  const BarChart = () => {
+    return (
+      <BarChart
+        style={{
+          width: "100%",
+          maxWidth: "300px",
+          maxHeight: "100px",
+          aspectRatio: 1.618,
+          isAnimationActive: { isAnimationActive },
+        }}
+        responsive
+        data={apiData}
+      >
+        <Bar dataKey="uv" fill="#8884d8" />
+      </BarChart>
+    );
+  };
+};
+
+export default BarChartType;
