b90934d10efcd8c6f105f311376cdc16258fed9b
Lucie Seng <lucie.sng@gmail.com>
2025-12-05 11:51:48 +0100

    feat : link homepage

--- src/assets/components/header.tsx
+++ src/assets/components/header.tsx
@@ -1,17 +1,25 @@
-const HeaderComponent = () => (
-  <header className="navbar bg-blue-700">
-    <div className="flex-1">
-      {/* <Link to="/analysesPage"> */}
-      <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl btn-ghost">
-        Accueil
-      </button>
-      {/* </Link> */}
-    </div>
-    <div className="flex-1">
-      <h1>Dataviz, vos données ciné à Paris</h1>
-    </div>
+import { useNavigate } from "react-router-dom";
 
-    <div className="flex-none"></div>
-  </header>
-);
-export default HeaderComponent;
+export default function HeaderComponent() {
+  const nav = useNavigate();
+  const navigate = () => {
+    nav("/Homepage");
+  };
+  return (
+    <header className="navbar bg-blue-700">
+      <div className="flex-1">
+        <button
+          onClick={navigate}
+          className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl btn-ghost"
+        >
+          Accueil
+        </button>
+      </div>
+      <div className="flex-1">
+        <h1>Dataviz, vos données ciné à Paris</h1>
+      </div>
+
+      <div className="flex-none"></div>
+    </header>
+  );
+}
